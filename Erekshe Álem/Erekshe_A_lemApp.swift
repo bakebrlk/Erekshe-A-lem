0ecc49dea243853a09f150879fffc61aff1aa156
Birlik Bekzat <birlik.b@mybuh.kz>
2025-11-07 14:49:44 +0500

    Initial Commit

---
+++ Erekshe Álem/Erekshe_A_lemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Erekshe_A_lemApp.swift
+//  Erekshe Álem
+//
+//  Created by bakebrlk on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Erekshe_A_lemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
